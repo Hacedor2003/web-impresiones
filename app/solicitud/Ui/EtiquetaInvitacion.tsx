8679d25113623776ecd4f370bbda604585342468
Hacedor2003 <bryanlenier@gmail.com>
2024-01-28 14:22:29 -0500

    Solucion error de produccion

--- app/solicitud/Ui/EtiquetaInvitacion.tsx
+++ app/solicitud/Ui/EtiquetaInvitacion.tsx
@@ -4,18 +4,18 @@ import './styles.css';
 
 export const EtiquetaInvitacion = () => {
 	const [index, setIndex] = useState(0);
-	const itemListRef = useRef(null);
+	const itemListRef = useRef<HTMLUListElement | null>(null);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			const itemList = itemListRef.current?.children;
-			const nextIndex = (index + 1) % itemList.length;
+			const itemList = itemListRef.current?.children as HTMLCollection;
+			const nextIndex = (index + 1) % (itemList?.length || 0);
 
-			for (let i = 0; i < itemList.length; i++) {
-				itemList[i].removeAttribute('id');
+			for (let i = 0; i < (itemList?.length || 0); i++) {
+				(itemList[i] as HTMLElement)?.removeAttribute('id');
 			}
 
-			itemList[nextIndex].setAttribute('id', 'activo');
+			(itemList[nextIndex] as HTMLElement)?.setAttribute('id', 'activo');
 
 			setIndex(nextIndex);
 		}, 1500);
@@ -23,30 +23,20 @@ export const EtiquetaInvitacion = () => {
 		return () => clearInterval(interval);
 	}, [index]);
 
+	const items = ['Bodas', 'Quinces', 'Cumpleaños', 'Bautizos'];
+
 	return (
 		<ul
 			className={styles.listaInvitaciones}
 			ref={itemListRef}>
-			<li
-				className={styles.itemListaInvitaciones}
-				id={index === 0 ? 'activo' : ''}>
-				<small>Bodas</small>
-			</li>
-			<li
-				className={styles.itemListaInvitaciones}
-				id={index === 1 ? 'activo' : ''}>
-				<small>Quinces</small>
-			</li>
-			<li
-				className={styles.itemListaInvitaciones}
-				id={index === 2 ? 'activo' : ''}>
-				<small>Cumpleaños</small>
-			</li>
-			<li
-				className={styles.itemListaInvitaciones}
-				id={index === 3 ? 'activo' : ''}>
-				<small>Bautizos</small>
-			</li>
+			{items.map((item, idx) => (
+				<li
+					key={idx}
+					className={styles.itemListaInvitaciones}
+					id={index === idx ? 'activo' : ''}>
+					<small>{item}</small>
+				</li>
+			))}
 		</ul>
 	);
 };
