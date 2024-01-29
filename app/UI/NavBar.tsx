a7a2fe7d038fe617c1da731207854c8fa4b148c9
Hacedor2003 <bryanlenier@gmail.com>
2024-01-29 10:41:10 -0500

    Arreglado estilos del navBar

--- app/UI/NavBar.tsx
+++ app/UI/NavBar.tsx
@@ -5,49 +5,7 @@ import { Acerca } from './Acerca';
 import { Contactar } from './Contactar';
 import { Horario } from './Horario';
 import { Ubicacion } from './Ubicacion';
-
-const BarNav = styled.div`
-	display: flex;
-	flex-direction: column-reverse;
-	flex-wrap: wrap;
-	align-content: center;
-	justify-content: center;
-	align-items: center;
-	background-color: white;
-	color: black;
-	padding: 2vh;
-	& > ul {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		align-content: center;
-		justify-content: space-around;
-		align-items: center;
-		list-style: none;
-		border: 2px solid black;
-		border-radius: 0px 0px 20px 20px;
-		background-color: black;
-		color: white;
-		& > li {
-			padding: 10px;
-			padding-left: 5vw;
-			padding-right: 5vw;
-			transition: 0.5s ease;
-			&:hover {
-				background-color: white;
-				color: black;
-				border-radius: 0px 0px 20px 20px;
-			}
-		}
-	}
-`;
-
-const Logo = styled.aside`
-	position: absolute;
-	top: 50vh;
-	left: 50vw;
-	z-index: 10;
-`;
+import estilos from './estilos.module.css'
 
 export const NavBar = ({ children }: { children: ReactNode }) => {
 	const [visibleComponent, setVisibleComponent] = useState('');
@@ -62,15 +20,15 @@ export const NavBar = ({ children }: { children: ReactNode }) => {
 
 	return (
 		<>
-			<BarNav>
-				<Logo>Logo</Logo>
+			<div className={estilos.BarNav}>
+				<aside className={estilos.Logo}>Logo</aside>
 				<ul>
 					<li onClick={() => handleComponentClick('Horario')}>Ver Horario</li>
 					<li onClick={() => handleComponentClick('Ubicacion')}>Ver Ubicación</li>
 					<li onClick={() => handleComponentClick('Contactar')}>Contactar</li>
 					<li onClick={() => handleComponentClick('Acerca')}>Acerca</li>
 				</ul>
-			</BarNav>
+			</div>
 			{visibleComponent === 'Acerca' && <Acerca visible={true} />}
 			{visibleComponent === 'Contactar' && <Contactar visible={true} />}
 			{visibleComponent === 'Horario' && <Horario visible={true} />}
