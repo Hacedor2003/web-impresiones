e2f18ab8d2cf2820806842b24f44f397e39b626d
Hacedor2003 <bryanlenier@gmail.com>
2024-01-29 10:51:01 -0500

    Optimizacion de estilos

--- app/invitaciones/page.tsx
+++ app/invitaciones/page.tsx
@@ -2,7 +2,6 @@
 import styles from './estyles.module.css';
 import classes from '../solicitud/Styles/styles.module.css';
 import Link from 'next/link';
-import { ContendorInvitaciones } from './UI/estilos';
 import Image from 'next/image';
 import { Main } from './UI/Ui';
 
@@ -10,8 +9,8 @@ export default function Page() {
 	return (
 		<Main>
 			<header className={classes.headerCard}>Todas las invitaciones:</header>
-			<div className={styles.contenedorInvitaciones}>
-				<ContendorInvitaciones>
+			<div className={styles.contenedorDeLasInvitaciones}>
+				<div className={styles.ContendorInvitaciones}>
 					<Link href={'/invitaciones/invitaciones_de_boda'}>
 						<Image
 							src='/perfil.jpg'
@@ -21,8 +20,8 @@ export default function Page() {
 						/>
 						<p>Invitaciones de Boda</p>
 					</Link>
-				</ContendorInvitaciones>
-				<ContendorInvitaciones>
+				</div>
+				<div className={styles.ContendorInvitaciones}>
 					<Link href={'/invitaciones/invitaciones_de_quince'}>
 						<Image
 							src='/perfil.jpg'
@@ -32,8 +31,8 @@ export default function Page() {
 						/>
 						<p>Invitaciones de Quince</p>
 					</Link>
-				</ContendorInvitaciones>
-				<ContendorInvitaciones>
+				</div>
+				<div className={styles.ContendorInvitaciones}>
 					<Link href={'/invitaciones/demas_invitaciones'}>
 						<Image
 							src='/perfil.jpg'
@@ -43,7 +42,7 @@ export default function Page() {
 						/>
 						<p>Demas Invitaciones</p>
 					</Link>
-				</ContendorInvitaciones>
+				</div>
 			</div>
 		</Main>
 	);
