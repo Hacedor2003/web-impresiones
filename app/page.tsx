7fea1c42442eb9a43592a7e054de8e555ae5b8f2
Hacedor2003 <bryanlenier@gmail.com>
2024-02-04 20:22:35 -0500

    Optimizacion de Estilos

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
 import { Container } from 'react-bootstrap';
 import Link from 'next/link';
 import './page.css';
+import { useEffect } from 'react';
 
 export default function Home() {
+	useEffect(() => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js').then((registration) => console.log('scope is: ', registration.scope));
+		}
+	}, []);
 	return (
 		<Container
 			className={'contenedor'}
