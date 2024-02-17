fcfa551ad404b8e6a01eed83fa3386515b4a39d0
Hacedor2003 <bryanlenier@gmail.com>
2024-02-16 23:56:56 -0500

    Actualizacion de las Vistas

--- app/UI/UiTarjeta.tsx
+++ app/UI/UiTarjeta.tsx
@@ -17,6 +17,26 @@ const Contenedor = styled.div`
 	flex-wrap: wrap;
 	justify-content: flex-start;
 	align-items: flex-start;
+	overflow: overlay;
+
+	@media screen and (max-width: 640px) {
+		background-color: white;
+		position: absolute;
+		right: 11vw;
+		top: 20vh;
+		height: 60vh;
+		width: 77vw;
+		z-index: 10;
+		border: 5px solid black;
+		display: flex;
+		align-content: flex-start;
+		color: black;
+		flex-direction: column;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+		align-items: flex-start;
+		overflow: overlay;
+	}
 `;
 export const UiTarjeta = ({ children, visible }: { children: ReactElement; visible: boolean }) => {
 	return (
